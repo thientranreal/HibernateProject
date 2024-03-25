3cb08b2dd8b0b2d9d2bffa5081fb773cb60b928a
thientranreal <thientt2612@gmail.com>
2024-03-25 13:48:09 +0700

    add sql

---
+++ src/main/java/com/project/Entity/ThanhVien.java
@@ -0,0 +1,27 @@
+package com.project.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "thanhvien")
+@Getter
+@Setter
+public class ThanhVien {
+    @Id
+    @Column (name = "MaTV")
+    private int maTV;
+    @Column (name = "HoTen")
+    private String hoTen;
+    @Column (name = "Khoa")
+    private String khoa;
+    @Column (name = "Nganh")
+    private String nganh;
+    @Column (name = "SDT")
+    private int sdt;
+}
